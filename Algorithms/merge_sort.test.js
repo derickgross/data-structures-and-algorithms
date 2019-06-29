af0aee7d27a7eaecb4ceac44e7f82aac8105a15d
Derick Gross <derick.gross@gmail.com>
2019-06-29 19:28:30 -0400

    add test to cover improper compare function

--- Algorithms/merge_sort.test.js
+++ Algorithms/merge_sort.test.js
@@ -20,7 +20,35 @@ test('without comparison function, sorts an array of words in alphabetical order
 	expect(output[3]).toBe("dirt");
 	expect(output[4]).toBe("eggplant");
 });
-test('with proper comparison function, sorts numbers low to high', () => {
+test('with proper comparison function, sorts an array of words in alphabetical order', () => {
+	const comparer = (first, second) => {
+		return first < second;
+	};
+
+	const input = ["eggplant", "banana", "dirt", "chocolate", "apple"];
+	const output = mergeSort(input, comparer);
+
+	expect(output[0]).toBe("apple");
+	expect(output[1]).toBe("banana");
+	expect(output[2]).toBe("chocolate");
+	expect(output[3]).toBe("dirt");
+	expect(output[4]).toBe("eggplant");
+});
+test('with proper comparison function, sorts an array of words in reverse alphabetical order', () => {
+	const comparer = (first, second) => {
+		return first > second;
+	};
+
+	const input = ["eggplant", "banana", "dirt", "chocolate", "apple"];
+	const output = mergeSort(input, comparer);
+
+	expect(output[0]).toBe("eggplant");
+	expect(output[1]).toBe("dirt");
+	expect(output[2]).toBe("chocolate");
+	expect(output[3]).toBe("banana");
+	expect(output[4]).toBe("apple");
+});
+test('with proper comparison function, sorts an array of numbers low to high', () => {
 	const comparer = (first, second) => {
 		return first < second;
 	};
@@ -34,7 +62,7 @@ test('with proper comparison function, sorts numbers low to high', () => {
 	expect(output[3]).toBe(4);
 	expect(output[4]).toBe(5);
 });
-test('with proper comparison function, sorts numbers high to low', () => {
+test('with proper comparison function, sorts an array of numbers high to low', () => {
 	const comparer = (first, second) => {
 		return first > second;
 	};
@@ -46,5 +74,19 @@ test('with proper comparison function, sorts numbers high to low', () => {
 	expect(output[1]).toBe(4);
 	expect(output[2]).toBe(3);
 	expect(output[3]).toBe(2);
+	expect(output[4]).toBe(1);
+});
+test('with improper comparison function, returns an array identical to input', () => {
+	const comparer = (first, second) => {
+		return "nonsense";
+	};
+
+	const input = [5,2,4,3,1];
+	const output = mergeSort(input, comparer);
+
+	expect(output[0]).toBe(5);
+	expect(output[1]).toBe(2);
+	expect(output[2]).toBe(4);
+	expect(output[3]).toBe(3);
 	expect(output[4]).toBe(1);
 });
